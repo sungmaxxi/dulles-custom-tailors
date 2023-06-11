ccd7b95066f7b52cc077706c3846506cb6e51d9e
Sung Yang <sungmaxxi@gmail.com>
2023-06-11 19:58:19 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `dulles-custom-tailors`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
